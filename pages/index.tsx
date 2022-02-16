ea0ea460d120dfcedb74ea8c6363b8860c251c65
kumagai-s <kumaga.yuki@servithink.co.jp>
2022-02-16 19:45:31 +0900

    styleの修正

--- pages/index.tsx
+++ pages/index.tsx
@@ -20,34 +20,36 @@ export default function IndexPage() {
   }
 
   return (
-    <div className="overflow-hidden max-w-[990px] mx-auto">
-      <div className='py-5 text-center'>
-        <div className='mb-6'>
-          <h1 className='text-3xl'>
-            ポケモンユナイト<br className='sm:hidden' />作戦会議室
-          </h1>
-        </div>
-        <div className='mb-6'>
+    <>
+      <div className="max-w-[990px] mx-auto">
+        <div className='py-5 text-center'>
           <div className='mb-6'>
-            <div className='inline-block text-left'>
-              <div className='mb-3'>
-                <h2 className='text-xl'>使い方</h2>
+            <h1 className='text-3xl'>
+              ポケモンユナイト<br className='sm:hidden' />作戦会議室
+            </h1>
+          </div>
+          <div className='mb-6'>
+            <div className='mb-6'>
+              <div className='inline-block text-left'>
+                <div className='mb-3'>
+                  <h2 className='text-xl'>使い方</h2>
+                </div>
+                <ol className='list-decimal list-inside'>
+                  <li>ルーム作成をクリック</li>
+                  <li>リンクをコピーして共有</li>
+                  <li>描いた内容を共有できます</li>
+                </ol>
               </div>
-              <ol className='list-decimal list-inside'>
-                <li>ルーム作成をクリック</li>
-                <li>リンクをコピーして共有</li>
-                <li>描いた内容を共有できます</li>
-              </ol>
+            </div>
+            <div>
+              <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={clickHandler}>
+                ルーム作成
+              </button>
             </div>
           </div>
-          <div>
-            <button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded" onClick={clickHandler}>
-              ルーム作成
-            </button>
-          </div>
         </div>
-        <Footer />
       </div>
-    </div>
+      <Footer />
+    </>
   )
 }
